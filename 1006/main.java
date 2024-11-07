0175a60d51d7c6a0c4af109848fc710ce7063e07
Matheus Barbosa <115056575+themattery@users.noreply.github.com>
2024-11-07 18:26:08 -0300

    beecrowd 1006 java

---
+++ 1006/main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner teclado = new Scanner(System.in);
+        double a, b, c, media;
+        
+        a = teclado.nextDouble();
+        b = teclado.nextDouble();
+        c = teclado.nextDouble();
+        media = ((a*2)+(b*3)+(c*5))/10;
+        
+        System.out.printf("MEDIA = %.1f\n", media);
+ 
+    }
+ 
+}
